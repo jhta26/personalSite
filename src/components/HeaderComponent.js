e57f467ca6f3d2f23cb4786f191c2cd60ad09542
jhta26 <hsujasonf@gmail.com>
2018-04-30 15:54:03 -0700

    fixing mobile nav

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -7,55 +7,51 @@ export default function HeaderComponent({ onCloseProject }) {
   };
 
   return (
-    <nav className="navigation white container m12 s12 z-depth-0">
-      <div className="nav-wrapper  white s12">
-
-        <Link to={"/"} className="black-text" onClick={_handleCloseProject}>
-          <a className="black-text white left nameLink">
-            JASON <span className="turq">H</span>{" "}
-            <img
-              src={require("./resources/hsu.png")}
-              alt="hsu"
-              height="30"
-              width="30"
-            />
+    <div>
+      <nav className="navigation white container m12 s12 z-depth-0">
+        <div className="nav-wrapper  white s12">
+          <Link to={"/"} className="black-text" onClick={_handleCloseProject}>
+            <a className="black-text white left nameLink">
+              JASON <span className="turq">H</span>{" "}
+              <img
+                src={require("./resources/hsu.png")}
+                alt="hsu"
+                height="30"
+                width="30"
+              />
+            </a>
+          </Link>
+        
+            <i className="material-icons">menu</i>
           </a>
-        </Link>
-          <a href="#" data-target="mobile-demo" className="sidenav-trigger"><i className="material-icons">menu</i></a>
-        <ul id="nav-mobile " className="right s12 hide-on-small-only">
-          <li>
-            <Link to={"/projects"} className="black-text">
-              PROJECTS
-            </Link>
-          </li>
-          <li>
-            <Link
-              to={"/aboutMe"}
-              className="black-text"
-              onClick={_handleCloseProject}
-            >
-              ABOUT ME
-            </Link>
-          </li>
-          <li>
-            <Link
-              to={"/resume"}
-              className="black-text"
-              onClick={_handleCloseProject}
-            >
-              RESUME
-            </Link>
-          </li>
-        </ul>
-      </div>
-    <ul className="sidenav" id="mobile-demo">
-    <li><a href="sass.html">Sass</a></li>
-    <li><a href="badges.html">Components</a></li>
-    <li><a href="collapsible.html">Javascript</a></li>
-    <li><a href="mobile.html">Mobile</a></li>
-  </ul>
-    </nav>
-
-
+          <ul id="nav-mobile " className="right s12">
+            <li>
+              <Link to={"/projects"} className="black-text">
+                PROJECTS
+              </Link>
+            </li>
+            <li>
+              <Link
+                to={"/aboutMe"}
+                className="black-text"
+                onClick={_handleCloseProject}
+              >
+                ABOUT ME
+              </Link>
+            </li>
+            <li>
+              <Link
+                to={"/resume"}
+                className="black-text"
+                onClick={_handleCloseProject}
+              >
+                RESUME
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+      
+    </div>
   );
 }
