4e9a657672653d633cb8e84a072945702630d6b3
jhta26 <hsujasonf@gmail.com>
2020-08-30 13:17:55 -0700

    updating react

--- src/components/AboutMeBodyComponent.js
+++ src/components/AboutMeBodyComponent.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Card, CardTitle } from "react-materialize";
+import { aboutMeText } from "./assets/aboutMe";
 export default function AboutMeBodyComponent() {
   return (
     <div className="AboutMeComponent container col">
@@ -14,22 +15,7 @@ export default function AboutMeBodyComponent() {
         </div>
         <div className="col m8">
           <h5>WHO AM I?</h5>
-          <p>
-            My name is Jason Hsu and I'm a software engineer with 2 1/2 years
-            react native experience. I love building web and mobile apps and I'm
-            excited to learn new technologies and frameworks. I got into tech
-            because I was attracted to an industry where I could really flex my
-            creative muscles. I'm attracted to the idea of going to work,
-            learning as much as I can and then coming home and using what I
-            learned on my own side projects. I most recently completed an
-            internship for s2e consulting where I helped create a money
-            management app using React Native, React.Js,Redux, Apollo GraphQL.
-            Prior to that I created a mobile app called Glance that lets people
-            coordinate there meetups by showing the location of all
-            participants. I'm currently looking for my next step. I would love
-            to join an innovative company where I could really make an impact
-            with my creativity.
-          </p>
+          <p>{aboutMeText.whoAmI}</p>
         </div>
       </div>
 
@@ -37,12 +23,7 @@ export default function AboutMeBodyComponent() {
       <div className="aboutMeComponent">
         <div className="col">
           <h5>WHERE AM I FROM?</h5>
-          <p>
-            I was born in NY, raised in Novi, MI, and I went to college at
-            Western Michigan University. I had to get out of the cold Michigan
-            weather so I moved to San Francisco in July of 2017 to pursue a
-            career in tech.
-          </p>
+          <p>{aboutMeText.whereAmIFrom}</p>
         </div>
         <div className="col">
           <img
---
+++ src/components/assets/aboutMe.js
@@ -0,0 +1,20 @@
+export let aboutMeText = {
+  whoAmI: `My name is Jason Hsu and I'm a software engineer with 2 1/2 years
+  react native experience. I love building web and mobile apps and I'm
+  excited to learn new technologies and frameworks. I got into tech
+  because I was attracted to an industry where I could really flex my
+  creative muscles. I'm attracted to the idea of going to work,
+  learning as much as I can and then coming home and using what I
+  learned on my own side projects. I most recently completed an
+  internship for s2e consulting where I helped create a money
+  management app using React Native, React.Js,Redux, Apollo GraphQL.
+  Prior to that I created a mobile app called Glance that lets people
+  coordinate there meetups by showing the location of all
+  participants. I'm currently looking for my next step. I would love
+  to join an innovative company where I could really make an impact
+  with my creativity.`,
+  whereAmIFrom: `I was born in NY, raised in Novi, MI, and I went to college at
+  Western Michigan University. I had to get out of the cold Michigan
+  weather so I moved to San Francisco in July of 2017 to pursue a
+  career in tech.`,
+};
